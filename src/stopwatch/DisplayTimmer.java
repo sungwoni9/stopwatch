546f7dd73a55d7aa3ab8f64a2c27bac9e9a5f97e
허성원 <hurdang99@naver.com>
2024-11-13 15:51:33 +0900

    Upadte run method add BufferedWritter

--- src/stopwatch/DisplayTimmer.java
+++ src/stopwatch/DisplayTimmer.java
@@ -1,34 +1,56 @@
 package stopwatch;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
 public class DisplayTimmer implements Runnable {
 
-	private Calendar cal = Calendar.getInstance();
+	private int startTime;
+	private int elapsedTime;
+
+	private static Calendar cal = Calendar.getInstance();
 	private TimeZone tz = TimeZone.getTimeZone("Asia/Seoul");
-	private SimpleDateFormat sdf = new SimpleDateFormat("HH : mm : ss");
+	private static SimpleDateFormat sdf = new SimpleDateFormat("HH : mm : ss");
 	
+	BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
 	public DisplayTimmer() {
+		startTime = (int) System.currentTimeMillis();
+		elapsedTime = 1;
 		sdf.setTimeZone(tz);
 	}
-	
+
 	public static DisplayTimmer getInstance() {
 		DisplayTimmer instance = new DisplayTimmer();
 		return instance;
+
 	}
-	
+
 	public void run() {
-		while (true) {
-			try {
+		try {
+			while (true) {
+				String currentTime = sdf.format(cal.getTime());
 				cal.setTimeInMillis(System.currentTimeMillis());
-				System.out.println("현재 시간: " + sdf.format(cal.getTime()));
+
+				writer.write("\n" + currentTime + " [" + elapsedTime++ + "]");
+				writer.flush();
+
 				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
 			}
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
+	public int getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
 }
