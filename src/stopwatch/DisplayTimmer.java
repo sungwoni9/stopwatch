f9f30a061a8424301895fe8033c132f7edb6ed1b
허성원 <hurdang99@naver.com>
2024-11-13 15:11:19 +0900

    Add : run method and getInstance

--- src/stopwatch/DisplayTimmer.java
+++ src/stopwatch/DisplayTimmer.java
@@ -1,5 +1,34 @@
 package stopwatch;
 
-public class DisplayTimmer {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class DisplayTimmer implements Runnable {
+
+	private Calendar cal = Calendar.getInstance();
+	private TimeZone tz = TimeZone.getTimeZone("Asia/Seoul");
+	private SimpleDateFormat sdf = new SimpleDateFormat("HH : mm : ss");
+	
+	public DisplayTimmer() {
+		sdf.setTimeZone(tz);
+	}
+	
+	public static DisplayTimmer getInstance() {
+		DisplayTimmer instance = new DisplayTimmer();
+		return instance;
+	}
+	
+	public void run() {
+		while (true) {
+			try {
+				cal.setTimeInMillis(System.currentTimeMillis());
+				System.out.println("현재 시간: " + sdf.format(cal.getTime()));
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
